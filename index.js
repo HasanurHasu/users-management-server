ebab0483179a1b21d0779b936336f23b8850ee9d
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-10-12 21:50:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+var cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+const users = [
+    {id: 1, name: 'Abu Bokkor', email: 'abubokkor@gmail.com'},
+    {id: 2, name: 'Mamun Islam', email: 'mamunislam@gmail.com'},
+    {id: 3, name: 'rohimuddin', email: 'rohimuddin@gmail.com'}
+]
+
+app.get('/', (req, res) => {
+    res.send('Users management server is running')
+})
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.post('/users', (req, res) =>{
+    console.log('post api hitting');
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT: ${port}`);
+})
